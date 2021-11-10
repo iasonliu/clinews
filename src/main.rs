a8595e48fbc075bfe3ea6807f081de6cfcede66f
Xiangxu <iasonliu@gmail.com>
2021-11-10 12:07:40 +0800

    add dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use colour::{dark_blue, yellow};
+use dotenv::dotenv;
 use std::error::Error;
 
 use newsapi::{get_articles, Articles};
@@ -10,9 +11,11 @@ fn render_articles(articles: &Articles) {
     }
 }
 fn main() -> Result<(), Box<dyn Error>> {
-    let url: &str =
-        "https://newsapi.org/v2/top-headlines?country=us&apiKey=3d17acbf6bc749ba908d040a2699089a";
-    let articles = get_articles(url)?;
+    dotenv().ok();
+    let api_key = std::env::var("API_KEY")?;
+    let url: &str = "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
+    let url = format!("{}{}", url, api_key);
+    let articles = get_articles(&url)?;
     render_articles(&articles);
     Ok(())
 }

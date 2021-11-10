417c4cf120d9c9e99a6564258858519220aa7475
Xiangxu <iasonliu@gmail.com>
2021-11-10 11:46:25 +0800

    add new lib

---
+++ newsapi/src/lib.rs
@@ -0,0 +1,19 @@
+use serde::Deserialize;
+use std::error::Error;
+
+#[derive(Deserialize, Debug)]
+pub struct Articles {
+    pub articles: Vec<Article>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Article {
+    pub title: String,
+    pub url: String,
+}
+
+pub fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
+    let response: String = ureq::get(url).call()?.into_string()?;
+    let articles: Articles = serde_json::from_str(&response)?;
+    Ok(articles)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,7 @@
 use colour::{dark_blue, yellow};
-use serde::Deserialize;
 use std::error::Error;
 
-#[derive(Deserialize, Debug)]
-struct Articles {
-    articles: Vec<Article>,
-}
-
-#[derive(Deserialize, Debug)]
-struct Article {
-    title: String,
-    url: String,
-}
-fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
-    let response: String = ureq::get(url).call()?.into_string()?;
-    let articles: Articles = serde_json::from_str(&response)?;
-    Ok(articles)
-}
+use newsapi::{get_articles, Articles};
 
 fn render_articles(articles: &Articles) {
     for a in &articles.articles {
